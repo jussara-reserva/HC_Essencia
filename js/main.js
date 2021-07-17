c1584d30f0f210fafafcee625d9ab999dd21903e
Jussara Rodrigues <jussara.rodrigues10@hotmail.com>
2021-07-17 18:08:46 -0300

    Adiciona o layout

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Contagem regressiva - Retirado do w3schools e adaptado (https://www.w3schools.com/howto/howto_js_countdown.asp)
+let dateEvent = new Date('Nov 11, 2021 00:00:00').getTime()
+
+// Atualiza a contagem a cada 1 segundo
+let x = setInterval(function () {
+  let dateNow = new Date().getTime()
+
+  let timeToEnd = dateEvent - dateNow
+
+  let days = Math.floor(timeToEnd / (1000 * 60 * 60 * 24))
+  let hours = Math.floor((timeToEnd % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  let minutes = Math.floor((timeToEnd % (1000 * 60 * 60)) / (1000 * 60))
+  let seconds = Math.floor((timeToEnd % (1000 * 60)) / 1000)
+
+  document.getElementById('days').innerHTML = days
+  document.getElementById('hours').innerHTML = hours
+  document.getElementById('minutes').innerHTML = minutes
+  document.getElementById('seconds').innerHTML = seconds
+
+  if (timeToEnd < 0) {
+    document.getElementById('days').innerHTML = 0
+    document.getElementById('hours').innerHTML = 0
+    document.getElementById('minutes').innerHTML = 0
+    document.getElementById('seconds').innerHTML = 0
+
+    clearInterval(x)
+    let dateText = document
+      .getElementById('home-box-left')
+      .getElementsByTagName('h3')[0]
+
+    dateText.innerHTML = 'O grande dia chegou! <br /> Descontos até 50% off!!!'
+    dateText.style.letterSpacing = '1px'
+    dateText.style.textTransform = 'none'
+
+    let discountText = document
+      .getElementById('home-box-left')
+      .getElementsByTagName('p')[0]
+    discountText.style.display = 'none'
+  }
+}, 1000)
